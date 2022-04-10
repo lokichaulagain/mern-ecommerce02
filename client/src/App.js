ad6408e1ae859102b76912e8ff9998b6176ae74d
lokendra <lokendrachaulagain803@gmail.com>
2022-04-10 20:26:52 +0545

    registerPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 // import Home from "./pages/home/Home";
 // import SingleProduct from "./pages/singleProduct/SingleProduct";
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <SingleProduct /> */}
-      <Login/>
+      {/* <Login/> */}
+      <Register />
     </div>
   );
 }
---
+++ client/src/pages/register/Register.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./register.scss"
+
+function Register() {
+    return (
+        <div className='registerCon'>
+            <div className="registerWrapper">
+                <span className='registerTitle'>Enter Credentials to Register</span>
+                <input className='regUsernameInput' type="email" placeholder="Email " />
+                <input className='regUsernameInput' type="text" placeholder="Username " />
+                <input className='regUsernameInput' type="password" placeholder="Create Password" />
+                <input className='regUsernameInput' type="password" placeholder="Confirm Password" />
+                <button className='registerBut'>Register</button>
+
+
+                <span className='alreadyHaveAcc'>Already have an account? LogIn</span>
+
+
+
+
+                <button className='registerBut'>LogIn</button>
+
+
+            </div>
+
+        </div>
+    )
+}
+export default Register
