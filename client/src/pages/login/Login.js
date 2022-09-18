3e88482e71da7682c7037309a5d64ce112d45182
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-09-18 11:14:00 +0545

    removed env and node

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,59 +1,62 @@
-import React from 'react'
-import "./login.scss"
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUser } from '../../redux/apiCalls';
-
-
-
-
+import React from "react";
+import "./login.scss";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../redux/apiCalls";
 
 function Login() {
+  //LOGIN ====================================>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
 
-    //LOGIN ====================================>
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const { isFetching, error } = useSelector(state => state.user);
+  const handleLogin = (e) => {
+    e.preventDefault();
 
+    //using login function  apiCalls
+    loginUser(dispatch, { username, password });
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
+  // console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
 
-        //using login function  apiCalls
-        loginUser(dispatch, { username, password })
-        
-    
-    }
+  return (
+    <div className="loginCon">
+      <form className="loginWrapper">
+        <input
+          className="usernameInput"
+          type="text"
+          placeholder="Username "
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="usernameInput"
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="loginBut"
+          type="submit"
+          onClick={handleLogin}
+          disabled={isFetching}
+        >
+          Login
+        </button>
 
-    // console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
+        {error && <span className="errorSpan">Something went wrong</span>}
 
-
-
-
-
-    return (
-        <div className='loginCon'>
-            <form className="loginWrapper">
-                <input className='usernameInput' type="text" placeholder="Username " name='username' onChange={(e) => setUsername(e.target.value)} />
-                <input className='usernameInput' type="password" name='password' placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                <button className='loginBut' type='submit' onClick={handleLogin} disabled={isFetching} >Login</button>
-
-                {error && <span className="errorSpan">Something went wrong</span>}
-
-                <div className="createAccAndForgotRow">
-                    <span className='dontHaveAcc'>Dont have an Account ? Create !</span>
-                    <span className='forgotPassword'>Forgot Password ?</span>
-
-                </div>
-
-                <button className='loginBut'>Register</button>
-
-
-            </form>
-
+        <div className="createAccAndForgotRow">
+          <span className="dontHaveAcc">Dont have an Account ? Create !</span>
+          <span className="forgotPassword">Forgot Password ?</span>
         </div>
-    )
+
+        <button className="loginBut">Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
